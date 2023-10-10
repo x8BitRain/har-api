53f06e34308e7a4a55a68fa646aa423b476ea4d9
mint <dashiell.bell@roomle.com>
2023-10-10 21:01:52 +0800

    feat: CLI interface

--- cli.ts
+++ cli.ts
@@ -1,16 +1,44 @@
 #!/usr/bin/env bun
 
+import { Command } from 'commander'
 import { setupServer } from './index.ts'
 
+const program = new Command()
+
 const config = {
-  har: '/Users/mint/Downloads/localhost.har',
+  har: '',
   port: 1565,
   debug: true,
-  apiUrl: 'https://api.roomle.com/v2',
+  apiUrl: '',
 }
 
-if (!config.apiUrl.endsWith('/')) {
+const startup = () => {
+  if (!config.har || !config.apiUrl) {
+    console.error('❌ You must specify a HAR file and an API URL')
+    program.help()
+    return
+  }
+
+  if (config.apiUrl && !config.apiUrl.endsWith('/')) {
     config.apiUrl += '/'
+  }
+
+  setupServer(config)
 }
 
-setupServer(config)
+program
+  .name('har-api')
+  .description('A simple HTTP server that serves a HAR file as an API')
+  .version('0.0.1')
+
+program
+  .argument('<API URL>', 'The API URL you want to mock responses from')
+  .argument('<HAR file>', 'The file path to your .har file')
+  .option('-p, --port', 'Port to run the server on', '1565')
+  .action((apiUrl, harDir) => {
+    config.har = harDir
+    config.apiUrl = apiUrl
+    startup()
+  })
+
+program.parse()
